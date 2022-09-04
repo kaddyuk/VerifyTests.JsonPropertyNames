962e990a8904038747b05ba4c277358c9a66a5b9
kaddyuk <kevin@cbmmgroup.com>
2022-09-04 12:13:47 +0100

    Add Example Test Case

--- VerifyTests.JsonPropertyNames/RegexExtensions.cs
+++ VerifyTests.JsonPropertyNames/RegexExtensions.cs
@@ -1,6 +1,76 @@
-﻿namespace VerifyTests.JsonPropertyNames;
+﻿using System.Diagnostics.Contracts;
+using System.Text.RegularExpressions;
 
-public class RegexExtensions
+namespace VerifyTests.JsonPropertyNames;
+
+public static class RegexExtensions
 {
-    
+    [Pure]
+    public static string ReplaceGroupValue(this Regex source, string input, string groupName, string destinationValue)
+    {
+        return ReplaceGroupValue(
+            source,
+            input,
+            m => m.Groups[groupName],
+            _ => destinationValue);
+    }
+
+    [Pure]
+    public static string ReplaceGroupValue(this Regex source, string input, int groupIdx, string destinationValue)
+    {
+        return ReplaceGroupValue(
+            source,
+            input,
+            m => m.Groups[groupIdx],
+            _ => destinationValue);
+    }
+
+    [Pure]
+    public static string ReplaceGroupValue(this Regex source, string input, string groupName, Func<string, string> destinationValueSelector)
+    {
+        return ReplaceGroupValue(
+            source,
+            input,
+            m => m.Groups[groupName],
+            destinationValueSelector);
+    }
+
+    [Pure]
+    public static string ReplaceGroupValue(this Regex source, string input, int groupIdx, Func<string, string> destinationValueSelector)
+    {
+        return ReplaceGroupValue(
+            source,
+            input,
+            m => m.Groups[groupIdx],
+            destinationValueSelector);
+    }
+
+    [Pure]
+    private static string ReplaceGroupValue(
+        Regex source,
+        string input,
+        Func<Match, Group> groupSelector,
+        Func<string, string> destinationValueSelector)
+    {
+        var matchResult = source.Matches(input);
+
+        if (matchResult.Count <= 0)
+        {
+            return input;
+        }
+
+        var text = input;
+
+        foreach (var group in matchResult.Select(groupSelector).OrderByDescending(p => p.Index))
+        {
+            var begin = group.Index > 0 ? text.Substring(0, group.Index) : string.Empty;
+            var end = group.Index + group.Length < text.Length
+                ? text[(group.Index + group.Length)..]
+                : string.Empty;
+            var destinationValue = destinationValueSelector.Invoke(group.Value);
+            text = $"{begin}{destinationValue}{end}";
+        }
+
+        return text;
+    }
 }
--- VerifyTests.JsonPropertyNames/UnitTest1.cs
+++ VerifyTests.JsonPropertyNames/UnitTest1.cs
@@ -1,10 +1,58 @@
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
 namespace VerifyTests.JsonPropertyNames;
 
+[UsesVerify]
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
 
+    private readonly VerifySettings _verifySettings = new ();
+    private readonly Regex _idFieldJsonRegex = new(@"^\t| *(?'idField'[\w]*Id): (?'idVal'[\w]*),?");
+    //private readonly Regex _idFieldXmlRegex = new();
+    
+    public UnitTest1()
+    {
+        _verifySettings.ScrubLinesWithReplace(s => _idFieldJsonRegex.ReplaceGroupValue(s, "idVal", "IdentityValue"));
+    }
+    /// <summary>
+    /// I expect the behaviour of ScrubLinesWithReplace to inject the full line into the delegate, including the property name, rather than the just value of the property on that line
+    /// It doesn't, this test case will fail.
+    /// I need to know the property name to accurately scrub out variable/changing values, like GUIDs and other Identity options. Not everyone uses GUIDs or Ints.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public Task Should_Remove_Id_Json_Lines_Using_Regex()
+    {
+        // Arrange
+        var obj = new ExampleObject()
+        {
+            Id = GenerateRandomString(10),
+            ForeignObjectId = GenerateRandomString(10)
+        };
+        
+        // Act
+        // Assert
+        return Verify(obj, _verifySettings);
+    }    
+    
+    public static string GenerateRandomString(int length, IEnumerable<char>? charSet = null)
+    {
+        charSet ??= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var charArray = charSet.Distinct().ToArray();
+        var result = new char[length];
+        for (var i = 0; i < length; i++)
+            result[i] = charArray[RandomNumberGenerator.GetInt32(charArray.Length)];
+        return new string(result);
     }
 }
+[Serializable]
+public record ExampleObject     
+{
+    public string Id { get; init; } = string.Empty;
+    public string ForeignObjectId { get; init; } = string.Empty;
+    
+    public string Name { get; init; } = "Name Of Person";
+} 
